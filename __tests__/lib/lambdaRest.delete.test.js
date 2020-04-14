93de151011ea0ce81b212c8a08b6db73dcaf7c95
Juan Antonio Cortes <jancorg@gmail.com>
2020-04-14 08:49:47 +0200

    first lambda rest interface implementation

---
+++ __tests__/lib/lambdaRest.delete.test.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const mod = require('../../lib/lambdaRest')
+const orm = require('../../lib/mensageoOrm')
+
+const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  }
+
+
+describe('lambdaRest.delete', () => {
+  beforeAll((done) => {
+
+    done()
+  })
+  afterEach( () => {
+      jest.clearAllMocks()
+  } )
+  it('should return 404 and no body', async () => {
+    const mockOrmDelete = jest.spyOn( orm, "delete" )
+                           .mockImplementation( (m, i, d) => new Promise( (res,rej) => rej('')))
+
+    const e = { 
+                body: JSON.stringify({ id: 0}),
+                pathParameters: { id: '0'} 
+              }
+
+    return mod.delete(e, null)
+              .then( r => {
+                            expect(r.statusCode).toBe(404)
+                            expect(r.headers).toEqual(corsHeaders)
+                            expect(mockOrmDelete).toHaveBeenCalled()
+                            expect(r.body).toBe(undefined)
+                          })
+  })
+
+  it('should return 204 and no body', async () => {
+    
+    const mockOrmDelete = jest.spyOn( orm, "delete" )
+                              .mockImplementation( (m, i, d) => new Promise( (res,rej) => res(1)))
+
+    const e = { 
+        body: JSON.stringify({ id: 0}),
+        pathParameters: { id: '0'} 
+      }
+    return mod.delete(e, null)
+              .then( r => {
+                            expect(r.statusCode).toBe(204)
+                            expect(r.headers).toEqual(corsHeaders)
+                            expect(mockOrmDelete).toHaveBeenCalled()
+                            expect(r.body).toBe(undefined)
+                          })
+  })
+
+})
---
+++ __tests__/lib/lambdaRest.list.test.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const mod = require('../../lib/lambdaRest')
+const orm = require('../../lib/mensageoOrm')
+
+const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  }
+
+describe('lambdaRest.list', () => {
+    beforeAll((done) => {
+        done()
+    })
+
+    afterEach( () => {
+        jest.clearAllMocks()
+    } )
+  
+    it('should return 404 and empty dataset', async () => {
+      const data = { results: [{}], total: 0}
+      const mockOrmFind = jest.spyOn( orm, "list" )
+                              .mockImplementation( (e, m) => new Promise( (res,rej) => rej(data)))
+  
+      const e = { queryStringParameters: { rangeStart: 0, rangeEnd: 10 } }
+  
+      return mod.list(e, null)
+                .then( r => {
+                              expect(r.statusCode).toBe(404)
+                              expect(r.headers).toEqual(corsHeaders)
+                              expect(mockOrmFind).toHaveBeenCalled()
+                              expect(JSON.parse(r.body)).toEqual(data)
+                            })
+    })
+  
+    it('should return 200 and strigified data', async () => {
+      
+      const data = { results: [{ id: 123 }]}
+  
+      const mockOrmFind = jest.spyOn( orm, "list" )
+                              .mockImplementation( (e, m) => new Promise( (res,rej) => res(data)))
+  
+      const e = { queryStringParameters: { rangeStart: 0, rangeEnd: 10, id: 123 } }
+  
+      return mod.list(e, null)
+                .then( r => {
+                              expect(r.statusCode).toBe(200)
+                              expect(r.headers).toEqual(corsHeaders)
+                              expect(mockOrmFind).toHaveBeenCalled()
+                              expect(JSON.parse(r.body)).toEqual(data)
+                            })
+    })
+  
+    // it('should return 400 and ask for required parameters', async () => {
+      
+    //     const data = { results: [{ id: 123 }]}
+    
+    //     const mockOrmFind = jest.spyOn( orm, "list" )
+    //                             .mockImplementation( (e, m) => new Promise( (res,rej) => res(data)))
+    
+    //     const e = { pathParameters: { id: '0' },
+    //                 queryStringParameters: {} }
+    
+    //     return mod.list(e, null)
+    //               .then( r => {
+    //                             expect(r.statusCode).toBe(400)
+    //                             expect(r.headers).toEqual(corsHeaders)
+    //                             expect(mockOrmFind.mock.calls.length).toBe(0)
+    //                             expect(JSON.parse(r.body)).toEqual({ msg: "range parameters are mandatory." })
+    //                           })
+    //   })
+
+  })
