fe78491d6884c918eefa30517f478e7ce1b2b8a3
Juan Antonio Cortes <jancorg@gmail.com>
2020-04-15 18:59:34 +0200

    default lambda handlers for api gateway

---
+++ handlers/CenterAdmin.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const m = require('../models/CenterAdmin')
+const a = require('../lib/lambdaRest')
+
+
+module.exports.get = async (e) => a.get(e, m)
+
+module.exports.list = async (e) => a.list(e, m)
+
+module.exports.post = async (e) => a.post(e, m)
+
+module.exports.patch = async (e) => a.patch(e, m)
+
+module.exports.delete = async (e) => a.delete(e, m)
