571dd62031b09d5988729d9bf800b595b0fcae7e
Juan Antonio Cortes <jancorg@gmail.com>
2020-04-23 17:39:48 +0200

    example for long size seeds

--- knex/seeds/centers.js
+++ knex/seeds/centers.js
@@ -2,7 +2,12 @@ const data = require('../../data/medical-centers/center')
 
 const insert_data = (knex,t,d) => knex(t).insert(d)
 
-exports.seed = function(knex, Promise) {
-    insert_data(knex,`center`, data)  
+exports.seed = async function(knex, Promise) {
+
+    data.map( x => insert_data(knex,`center`, data))
+
+    for (i in data) { await insert_data(knex,`center`, data[i]) }
+
+    return Promise
 }
 
